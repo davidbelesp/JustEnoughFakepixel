1c80e7155e850fe7f58328a06cab9a35ef0dc362
Whispering <ginafro240810@gmail.com>
2026-05-30 15:22:37 +0530

    change: bump version to 1.0.1-hotfix

--- src/main/java/io/hamlook/aetheria/Aetheria.java
+++ src/main/java/io/hamlook/aetheria/Aetheria.java
@@ -28,7 +28,7 @@ public class Aetheria {
 
     public static final String MODID = "aetheria";
     public static final String NAME = "Aetheria";
-    public static final String VERSION = "1.0.0-release";
+    public static final String VERSION = "1.0.1-hotfix";
 
     public static ATHRConfig config;
     public static Logger logger;
